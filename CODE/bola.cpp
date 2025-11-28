88468b4a814bd395ddbf5a078755d849a48182c9
habib aq <habibalqomairi@gmail.com>
2025-11-28 23:09:16 +0700

    tes kerangka

---
+++ CODE/bola.cpp
@@ -0,0 +1,9 @@
+#include <bola.h>
+Bola::Bola(int x, int y) {
+    
+}
+
+void Bola::gerak (int gx, int gy) {
+    x += gx;
+    y += gy;
+}
---
+++ CODE/bola.h
@@ -0,0 +1,28 @@
+#ifndef BOLA_H
+#define BOLA_H
+
+#include <cstdint>
+#include <cstdlib>
+
+void gerak();
+class Bola {
+    public: 
+    int x;
+    int y;
+    Bola(int x, int y);
+    void gerak (int gx, int gy);
+};
+
+
+
+
+
+
+
+void gerak(){ 
+    
+}
+
+
+#endif // BOLA_H
+
